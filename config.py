7c9f626a4c83db76737bfccd962694966871b5f5
lgwza <471819485@qq.com>
2024-05-15 09:26:10 +0000

    .

--- config.py
+++ config.py
@@ -1,5 +1,5 @@
 import subprocess
-import json
+from key_gen import get_key
 
 try:
     command_dev = "ip -6 route get 2001:4860:4860::8888 | grep -oP '(?<=dev )\S+'"
@@ -25,18 +25,18 @@ source_mac = result_mac
 source_iface = result_dev
 dst_address = "2402:f000:6:1e00::232"
 
-source_saddr_spoofable = False
-source_daddr_spoofable = True
-dst_saddr_spoofable = False
-dst_daddr_spoofable = True
+source_saddr_spoofable = True # 源端源地址可搭载信息——源端可伪造源地址，对端需接收，源端可发送
+source_daddr_spoofable = True # 源端目的地址可搭载信息——对端拥有别名前缀，对端需接收，源端可发送
+dst_saddr_spoofable = False # 对端源地址可搭载信息——对端可伪造源地址，对端可发送，源端需接收
+dst_daddr_spoofable = True # 对端目的地址可搭载信息——源端拥有别名前缀，对端可发送，源端需接收
 
-mode = 'T'
+mode = 'I'
 
-key = b'abcd3333'
+key = get_key()
 initial_message = b'\x00\x01\x02\x03\x04\x05\x06\x07'
 SYN_text = b'\x01\x02\x03\x04\x05\x06\x07\x08'
 SYN_ACK_text = b'\x01\x02\x03\x04\x01\x02\x03\x04'
-ACK_text = b'\x08\x07\x06\x05\x04\x03\x02\x01'
+ACK_text = b'\x08\x07\x06\x05\x08\x07\x06\x05'
 CLOSED = 0
 LISTEN = 1
 SYN_SENT = 2
@@ -48,4 +48,3 @@ CLOSE_WAIT = 7
 CLOSING = 8
 LAST_ACK = 9
 TIME_WAIT = 10
-status = LISTEN
---
+++ key_gen.py
@@ -0,0 +1,49 @@
+from Crypto.Cipher import ChaCha20
+from datetime import datetime, timedelta, timezone
+import os
+import hashlib
+
+def get_current_time_east_8():
+    # 获取当前UTC时间
+    now_utc = datetime.now()
+    # 设置东八区时区
+    east_8 = timezone(timedelta(hours=8))
+    # 转换为东八区时间并格式化为YYYYMMDDHHMM
+    now_east_8 = now_utc.astimezone(east_8)
+    formatted_time = now_east_8.strftime('%Y%m%d%H%M')
+    return formatted_time.encode()  # 转换为字节串
+
+def encrypt_time_with_chacha20(time_bytes, key):
+    # 生成随机nonce
+    nonce = b'\xbf\xcc\xe4\x81\xfc\xee2^:\xdd\xd4F'
+    # 创建ChaCha20加密器对象
+    cipher = ChaCha20.new(key=key, nonce=nonce)
+    # 加密数据
+    encrypted_data = cipher.encrypt(time_bytes)
+    return nonce + encrypted_data  # 返回nonce和密文，解密时需要nonce
+
+def md5_hash(bytes_string):
+    # 创建md5对象
+    md5 = hashlib.md5()
+    # 更新md5对象
+    md5.update(bytes_string)
+    # 获取md5值
+    return md5.digest()
+
+def get_key():
+    # chacha20 key 必须为32字节
+    key = b'}\x0c\xf3ufE\x94\xfaT\xb7\x8d\x81\xb6\x1b~\xe1\x81\x88\xbd\xfc\xf5\xa4\xbf\xb5\x98\x1d\xa2\x93iq#\x0b'
+
+    # 获取当前时间并格式化
+    current_time_bytes = get_current_time_east_8()
+    # print(current_time_bytes)
+    # 加密时间
+    encrypted_time = encrypt_time_with_chacha20(current_time_bytes, key)
+
+    # 输出前8位
+    ret_key = md5_hash(encrypted_time)[:8]
+    print(ret_key)
+    return ret_key
+
+if __name__ == '__main__':
+    get_key()
